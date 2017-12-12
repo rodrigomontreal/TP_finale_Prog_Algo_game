96f1c058e801d061cd98f028e8ff065fc9887a2e
Rodrigo Correa <rodrigomottanet@yahoo.com.br>
2017-12-12 08:18:50 -0500

    v0

--- TP_pgalgo_finale/TP_pgalgo_finale/ViewController.swift
+++ TP_pgalgo_finale/TP_pgalgo_finale/ViewController.swift
@@ -16,8 +16,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var mur_haut: UIView!
     @IBOutlet weak var mur_bas: UIView!
     @IBOutlet weak var balle: UIView!
-
-//_____________
+    @IBOutlet weak var game_over: UIView!
+    @IBOutlet weak var finger_mover: UIView!
+    
+    
+    //_____________
     var objet_bounce: Bounce!
     var cos: Double!
     var sin: Double!
@@ -39,18 +42,30 @@ class ViewController: UIViewController {
         let degres: Double = Double( arc4random_uniform(360))
         cos = __cospi(degres / 180)
         sin = __sinpi(degres / 180)
-        aTimer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(animation), userInfo: nil, repeats: true)
+        aTimer = Timer.scheduledTimer(timeInterval: 0.002, target: self, selector: #selector(animation), userInfo: nil, repeats: true)
     }
         //_______________
        @objc func animation() {
             balle.center.x += CGFloat(cos)
             balle.center.y += CGFloat(sin)
+        
+        if balle.frame.intersects(game_over.frame) {
+            aTimer.invalidate()
+            aTimer = nil
+        }
             
             sin = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[0]
             cos = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[1]
         }
-        
-       
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        let touch : UITouch = touches.first!
+        if touch.view == finger_mover {
+            finger_mover.center.x = touch.location(in: self.view).x
+            mur_bas.center.x = finger_mover.center.x
+        }
+    }
+    
     
 //________________
 }
