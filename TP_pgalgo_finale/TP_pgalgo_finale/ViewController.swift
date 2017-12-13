10da767c467f70eb0b69791bcc54ac6dca0d0fca
Rodrigo Correa <rodrigomottanet@yahoo.com.br>
2017-12-13 18:06:16 -0500

    v1

--- TP_pgalgo_finale/TP_pgalgo_finale/ViewController.swift
+++ TP_pgalgo_finale/TP_pgalgo_finale/ViewController.swift
@@ -18,7 +18,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var balle: UIView!
     @IBOutlet weak var game_over: UIView!
     @IBOutlet weak var finger_mover: UIView!
+    //-------------
+    //Variables Bounce1
+    @IBOutlet weak var mg: UIView!
+    @IBOutlet weak var md: UIView!
+    @IBOutlet weak var mb: UIView!
+    @IBOutlet weak var mh: UIView!
+
+    //----------
     
+    @IBOutlet weak var game_timerLabel: UILabel!
+    
+    
+    //---------------
+    var objet_bounce1: Bounce!
+    
+    
+    var game_timer: Timer!
+    var sec = 0
     
     //_____________
     var objet_bounce: Bounce!
@@ -31,9 +48,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         objet_bounce = Bounce(ball: balle, left_window: mur_gauche, right_window: mur_droit, top_window: mur_haut, bottom_window: mur_bas)
-        balle.layer.cornerRadius = 12.5
-        lancerAnimation()
+        balle.layer.cornerRadius = 25
         
+        objet_bounce1 = Bounce(ball: balle, left_window: mg, right_window: md, top_window: mh, bottom_window: mb)
+       
+        
+        lancerAnimation()
     }
 
     
@@ -43,12 +63,30 @@ class ViewController: UIViewController {
         cos = __cospi(degres / 180)
         sin = __sinpi(degres / 180)
         aTimer = Timer.scheduledTimer(timeInterval: 0.002, target: self, selector: #selector(animation), userInfo: nil, repeats: true)
+        game_timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(keep), userInfo: nil, repeats: true)
     }
         //_______________
+    
+    @objc func keep(){
+        sec += 1
+        game_timerLabel.text = "\(sec) seconds..."
+    }
+    
+    
+    
        @objc func animation() {
+        print("test")
             balle.center.x += CGFloat(cos)
             balle.center.y += CGFloat(sin)
-        
+        // boucle pour arreter le timer du jeu
+//        if sec == 10 {
+//        aTimer.invalidate()
+//        aTimer = nil
+//        game_timer.invalidate()
+//        game_timer = nil
+//        sec = 0
+//        }
+        //-----------
         if balle.frame.intersects(game_over.frame) {
             aTimer.invalidate()
             aTimer = nil
@@ -56,6 +94,13 @@ class ViewController: UIViewController {
             
             sin = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[0]
             cos = objet_bounce.returnCosSinAfterTouch(sin: sin, cos: cos)[1]
+        
+        // bounce 1 animation
+            sin = objet_bounce1.returnCosSinAfterTouch(sin: sin, cos: cos)[0]
+            cos = objet_bounce1.returnCosSinAfterTouch(sin: sin, cos: cos)[1]
+        
+        
+        
         }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
